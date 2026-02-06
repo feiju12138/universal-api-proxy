2e9919a07adfcdeafce3d0708800b72080fc8856
feiju12138 <49089085+feiju12138@users.noreply.github.com>
2026-02-06 16:41:32 +0800

    Refactor local-dev.js for improved API handling

--- local-dev.js
+++ local-dev.js
@@ -6,35 +6,40 @@ const API_TOKEN = process.env.API_TOKEN || "";
 const REQUEST_TIMEOUT = Number(process.env.REQUEST_TIMEOUT) || 10;
 
 if (!TARGET_API_URL) {
-  const errorMsg = "local-env: env TARGET_API_URL is require";
+  const errorMsg = "local-env: env TARGET_API_URL is required";
   console.error(errorMsg);
   throw new Error(errorMsg);
 }
 
 // 引入Nodejs的内置http模块
-let http = require("http");
+const http = require("http");
 
 // 创建服务器
-let server = http.createServer(function (request, response) {
-
-  response.setHeader("Content-Type", "application/json;charset=utf-8");
-
-  if (request.url === "/") {
-    response.end("ok");
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
+    case "/":
+      response.setHeader("Content-Type", "text/plain;charset=utf-8");
+      response.end("ok");
+      break;
+    case "/api":
+      fetchTargetApi(TARGET_API_URL, API_TOKEN, REQUEST_TIMEOUT).then((result) => {
+        response.setHeader("Content-Type", "application/json;charset=utf-8");
+        response.end(result);
+      }).catch((err) => {
+        response.statusCode = 500;
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.end(err.message);
+      });
+      break;
+    default:
+        response.writeHead(404, { "Content-Type": "text/plain;charset=utf-8" });
+        response.end("404");
+        break;
   }
-
-  if (request.url === "/api") {
-    fetchTargetApi(TARGET_API_URL, API_TOKEN, REQUEST_TIMEOUT).then((result) => {
-      response.end(result);
-    }).catch((error) => {
-      response.end(error);
-    });
-  }
-
 });
 
-// 设置监听端口，默认为80端口
-server.listen(80, function () {
-  console.log("local-dev: server is running: http://127.0.0.1:80");
-  console.log("local-dev: server(api) is running: http://127.0.0.1:80/api");
+// 设置监听端口
+server.listen(9000, function () {
+  console.log("local-dev: server is running: http://127.0.0.1:9000");
+  console.log("local-dev: server(api) is running: http://127.0.0.1:9000/api");
 });
