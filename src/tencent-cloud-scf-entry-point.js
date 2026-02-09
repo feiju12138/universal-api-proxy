b2b89512e672aa9d63eedb6ff46872386221ed9c
feiju12138 <49089085+feiju12138@users.noreply.github.com>
2026-02-09 20:38:53 +0800

    Delete src/tencent-cloud-scf-entry-point.js

--- src/tencent-cloud-scf-entry-point.js
+++
@@ -1,8 +0,0 @@
-const app = require("./express/app");
-
-const PORT = 9000;
-
-app.listen(PORT, () => {
-  console.log(`Server start on http://127.0.0.1:${PORT}`);
-  console.log(`Api Server start on http://127.0.0.1:${PORT}/api`);
-});
