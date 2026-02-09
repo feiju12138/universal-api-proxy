4e54ba7898146067ca06133d6080ab2c4d21783b
feiju12138 <49089085+feiju12138@users.noreply.github.com>
2026-02-09 12:50:55 +0800

    Delete src/app.js

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const app = require("./index.js");
-
-// 腾讯云云函数，只能监听 9000 端口
-const PORT = process.env.PORT || 9000;
-
-app.listen(PORT, () => {
-  console.log(`Server start on http://127.0.0.1:${PORT}`);
-  console.log(`Api Server start on http://127.0.0.1:${PORT}/api`);
-});
