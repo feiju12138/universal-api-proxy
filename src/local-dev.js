ea7f432b5caedb7de47993bc6b45abb468cce237
feiju12138 <49089085+feiju12138@users.noreply.github.com>
2026-02-10 09:32:13 +0800

    Update local-dev.js

--- src/local-dev.js
+++ src/local-dev.js
@@ -1,8 +1,8 @@
 const app = require("./express/app");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server start on http://127.0.0.1:${PORT}`);
   console.log(`Api Server start on http://127.0.0.1:${PORT}/api`);
 });
